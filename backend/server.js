863dbb13e7cb5844363099ca6a36a6a9b6922181
Antoine COULON <a.antoinecoulon@gmail.com>
2025-03-27 22:44:22 +0100

    Feature: login with token

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
+const JWT_SECRET = "VC31XPrKgBgihhGpuD89NtCjWS" // ToDo: à cacher
 
 app.use(cors({
     origin: "http://localhost:4200",
@@ -35,14 +37,34 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
+// Quand besoin de vérifier token:
+const authMiddleware = (req, res, next) => {
+    const token = req.header('Authorization').split(' ')[1];
+
+    if (!token) return res.status(401).json({ message: "Access denied, no token provided."});
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch(error) {
+        console.error('Token verification error:', error);
+        res.status(401).json({ message: "Invalid token" });
+    }
+};
+
 // Routes
 app.get("/", (req, res) => {
     res.send('Hello from Node.js');
 });
 
-app.get("/users", async (req, res) => {
-    const users = await User.find();
-    res.json(users);
+app.get("/users", authMiddleware, async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({ message: "Protected", user: req.user, users });
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
 });
 
 app.post("/register", async (req, res) => {
@@ -55,13 +77,18 @@ app.post("/login", async (req, res) => {
     try {
         const { username, password } = req.body;
         let user = await User.findOne({ username });
-        if (!user) return res.status(400).json({ message: "Invalid username: not found."})
+        if (!user) return res.status(400).json({ message: "Invalid username: not found."});
 
+        const isMatch = await password == user.password;
+        if (!isMatch) return res.status(400).json({ message: "Invalid password."});
 
+        const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
     }
-    
-    
-    })
+});
 
 // Server
 app.listen(PORT, () => {
