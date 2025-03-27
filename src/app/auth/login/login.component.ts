747811d6bf27a854a8bb1bc0734242aab7ba455a
Antoine COULON <a.antoinecoulon@gmail.com>
2025-03-27 16:52:54 +0100

    Waiting for server + service finished

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterLink, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  private readonly authService: AuthService = inject(AuthService)
+
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  })
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value
+      this.authService.checkCredentials(username, password)
+    } else {
+      console.log('Invalid form completion.'); 
+    }
+  }
+}
