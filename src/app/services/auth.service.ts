299e933e2f510315f3fb88640ddd1d1b10e479fa
Antoine COULON <a.antoinecoulon@gmail.com>
2025-03-27 16:52:27 +0100

    ToDo: adapt login with server logic

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,8 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../models/user';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 const API_BASE_URL = environment.auth_api
 
@@ -12,6 +13,14 @@ const API_BASE_URL = environment.auth_api
 export class AuthService {
   
   private readonly http: HttpClient = inject(HttpClient)
+  private readonly router: Router = inject(Router)
+
+  private readonly isAuthenticatedSubject = new BehaviorSubject<boolean>(false)
+  isAuthenticated$ = this.isAuthenticatedSubject.asObservable()
+
+  headers: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
 
   getUsers() {
     return this.http.get<User[]>(`${API_BASE_URL}users`)
@@ -23,12 +32,31 @@ export class AuthService {
       email: email,
       password: password
     };
-
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
-    });
-
+    const headers = this.headers
     return this.http.post(`${API_BASE_URL}register`, newUser, {headers})
   }
 
+  login(username: string, password: string) {
+    
+    
+    // const loggedUser = `logged-${username}`
+    // this.getUsers().subscribe(
+    //   (registeredUsers: User[]) => {
+
+    //     registeredUsers.forEach(user => {
+    //       if (user.username === username && user.password === password) {
+    //         this.isAuthenticatedSubject.next(true) 
+    //       }
+    //     })
+
+    //     if (this.isAuthenticated$) {
+    //       console.log("Login successful!");
+    //       sessionStorage.setItem("loggedUser", loggedUser)
+    //       this.router.navigate(["/"])
+    //     } else {
+    //       console.error("Error on login");
+    //     }
+    //   }
+    // )
+  }
 }
