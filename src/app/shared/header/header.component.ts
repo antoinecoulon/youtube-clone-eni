cf68b7c52534df355c57aaf6e0dcddd08ce7abc0
Antoine COULON <a.antoinecoulon@gmail.com>
2025-03-28 09:14:55 +0100

    Feature: logged or not logged

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,7 @@ import { RouterLink } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  isAuthenticated$ = inject(AuthService).isAuthenticated$
 
 }
