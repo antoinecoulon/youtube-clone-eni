34623a092afc00eb7c814cf758dd944fd6f08e87
Antoine COULON <a.antoinecoulon@gmail.com>
2025-03-28 16:25:51 +0100

    Add logo + username

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,17 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  loggedUser?: string
+  logoPath: string = "/logo.png"
 
   isAuthenticated$ = inject(AuthService).isAuthenticated$
-private readonly authService: AuthService = inject(AuthService)
+  private readonly authService: AuthService = inject(AuthService)
+
+  ngOnInit() {
+    if (localStorage.getItem("username")) {
+      this.loggedUser = localStorage.getItem("username")!
+    }
+  }
 
   logout() {
     this.authService.logout()
