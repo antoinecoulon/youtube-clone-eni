df80b7135a71e3af6e24c5a4f139cad052a7ccc1
Antoine COULON <a.antoinecoulon@gmail.com>
2025-03-28 14:01:42 +0100

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
+import { SearchComponent } from './pages/search/search.component';
+import { PlayerComponent } from './pages/player/player.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
+
+    { path: 'search', component: SearchComponent},
+    { path: 'player/:id', component: PlayerComponent }
 ];
