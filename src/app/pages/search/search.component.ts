1555ca29e3761f4083c21a964443f6b936a047b0
Antoine COULON <a.antoinecoulon@gmail.com>
2025-03-28 16:26:52 +0100

    Add loader

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,17 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, Signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { YtApiService } from '../../services/yt-api.service';
 import { Item, YtSearchApi } from '../../models/yt-search-api';
 import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search',
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-  searchResult?: Item[]  
+  searchResult?: Item[]
+  isLoading: Signal<boolean> = signal(false)
 
   private readonly router: Router = inject(Router)
   private readonly ytApiService: YtApiService = inject(YtApiService)
@@ -22,16 +24,20 @@ export class SearchComponent {
   })
 
   onSubmit() {
+    this.isLoading = signal(true)
+
     const searchParam = this.searchForm.value
     console.log(searchParam.search);
     
     this.ytApiService.search(searchParam.search).subscribe({
       next: (response: YtSearchApi) => {
-        this.searchResult = response.items 
+        this.searchResult = response.items
+        this.isLoading = signal(false)
       },
       error: (error: Error) => {
         console.error(error);        
       }
     })
   }
+  
 }
